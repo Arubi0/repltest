06318827b53e2516f0bda5dc252d53679ea3bae0
Arubi0 <arubiojr@cps.edu>
2024-09-06 13:28:26 +0000

    Added another question

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 name = input("what our name? ")
 print ("Your name is " + name)
+# typing things out 
+
+#class =  input("what is your favorite class? ")
+# print ("your favorite class is? " + class )
